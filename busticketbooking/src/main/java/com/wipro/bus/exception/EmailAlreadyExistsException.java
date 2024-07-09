34b64afea7da4d67811161b4aa4a51e7449076d8
Himanshu956 <kamanehimanshu76@gmail.com>
2024-07-09 16:45:03 +0530

    Final Back-end code

--- busticketbooking/src/main/java/com/wipro/bus/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.wipro.bus.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- busticketbooking/src/main/java/com/wipro/bus/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wipro.bus.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.bus.entities.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-  
-}
